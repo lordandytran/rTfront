cff9782a6492f98d64dbc89abf14d4109f27df8a
Andy <thematulaaklives.com>
2017-05-22 21:25:16 -0700

    Started frontend ui

--- index.php
+++ index.php
@@ -1,19 +1,48 @@
 <?php
+    require 'connect.php';
     require 'rpccalls.php';
 ?>
 <html>
     <head>
     </head>
     <body>
-    <?php
-        $arr = getDownloadList();
-        print_r($arr);
-        foreach($arr as $val) {
-            //print($val . " ". getName($val) . " " . getPriority($val) . " " . getSize($val) . " " . getState($val));
-            print_r(getSize($val));
-        }
-        unset($val);
-    ?>
+        <table style="width:100%">
+            <tr>
+                <th>Name</th>
+                <th>Size</th>
+                <!--<th>Done</th>
+                <th>Status</th>
+                <th>Seeds</th>
+                <th>Peers</th>-->
+                <th>Down Speed</th>
+                <th>Up Speed</th>
+                <!--<th>ETA</th>-->
+                <th>Ratio</th>
+                <th>Hash</th>
+            </tr>
+            <?php
+                $arr = getDownloadList();
+                foreach($arr as $val) {
+                    echo '<tr>';
 
+                    echo '<td>' . getName($val) . '</td>';
+
+                    echo '<td>' . getSize($val) . '</td>';
+
+                    echo '<td>' . getDownRate($val) . '</td>';
+
+                    echo '<td>' . getUpRate($val) . '</td>';
+
+                    echo '<td>';
+                    printf("%.2f",getRatio($val));
+                    echo '</td>';
+
+                    echo '<td>' . $val . '</td>';
+
+                    echo '</tr>';
+                }
+                unset($val);
+            ?>
+        </table>
     </body>
 </html>
--- rpccalls.php
+++ rpccalls.php
@@ -1,29 +1,83 @@
 <?php
-    require 'connect.php';
-
     function call($method, $args) {
         return sendRequest(array('method'=>$method, 'args'=>$args));
     }
 
-    //Returns the download list (hashed torrent name) as an array.
+    function sizeToString($size) {
+        $str = (string)$size; //Casting with abandon
+        $dec = $str[0] . "." . $str[1] . $str[2];
+        if($size >= 1000000000000) {
+            return $dec . " TB";
+        }
+        if($size >= 1000000000) {
+            return $dec . " GB";
+        }
+        if($size >= 1000000) {
+            return $dec . " MB";
+        }
+        if($size >= 1000) {
+            return $dec . " KB";
+        }
+        return $size . " bytes";
+    }
+
+    //returns download list as array
     function getDownloadList() {
         return call('download_list', "");
     }
 
-    //Returns the name of torrent given the hashed torrent name.
+    //returns name of torrent
     function getName($hash) {
-        return call('d.get_name', $hash);
+        return call('d.get_name', $hash)[0];
     }
 
+    //returns the priority of the torrent
     function getPriority($hash) {
-        return call('d.priority_str', $hash);
+        return call('d.priority_str', $hash)[0];
     }
 
+    //returns size of the torrent
     function getSize($hash) {
-        return call('d.size_bytes', $hash);
+        $size = call('d.size_bytes', $hash)[0];
+        return sizeToString($size);
+
     }
 
-    function getState($hash) {
-        return call('d.state', $hash);
+    //returns the current upload rate of torrent
+    function getUpRate($hash) {
+        $rate = call('d.up.rate', $hash)[0];
+        return sizeToString($rate) . "/s";
     }
+
+    //returns the current download rate of torrent
+    function getDownRate($hash) {
+        $rate = call('d.down.rate', $hash)[0];
+        return sizeToString($rate) . "/s";
+    }
+
+    //returns seed ratio of torrent
+    function getRatio($hash) {
+        return call('d.ratio', $hash)[0];
+    }
+
+    //stops given torrent
+    function stopTorrent($hash) {
+        call('d.stop', $hash);
+    }
+
+    //starts given torrent
+    function startTorrent($hash) {
+        call('d.start', $hash);
+    }
+
+    //erases given torrent
+    function eraseTorrent($hash) {
+        call('d.erase', $hash);
+    }
+
+    //loads and starts torrent
+    function createTorrent($link) {
+        call('load_start', $link);
+    }
+
 ?>
